3b06c3b183be21a10bde131d095123cda267dcff
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-09-09 02:08:27 +0530

    creating complex html structue using plain react.

--- 01-Inception/script.js
+++ 01-Inception/script.js
@@ -3,12 +3,31 @@
 //   {},
 //   "I have created an h2 tag using React Library"
 // );
-const para = React.createElement(
-  "p",
-  { id: "para" },
-  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?"
-);
+
+/*
+    <div id="parent"> 
+      <div id="child1">
+        <h1>I am an H1 Tag of child 1</h1>
+        <h1>I am an H2 Tag of child 1</h1>
+      </div>
+      <div id="child2">
+        <h1>I am an H1 Tag of child 2</h1>
+        <h1>I am an H2 Tag of child 2</h1>
+      </div>
+    </div>
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, " I am an H1 Tag of CHild 1"),
+    React.createElement("h2", {}, " I am an H2 Tag of CHild 1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, " I am an H1 Tag of CHild 2"),
+    React.createElement("h2", {}, " I am an H2 Tag of CHild 2"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(para);
+root.render(parent);
