7240964c8e60130c39d95cf44c50a91724bd55e0
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-09-10 00:33:56 +0530

    Igniting our app 1

---
+++ 01-Inception/README.md
@@ -0,0 +1,9 @@
+# Assignment - 01 Inception
+
+### 1 - What is Emmet ?
+
+Emmet is a web development tool and abbreviation engine that helps web developers write HTML and CSS code more quickly and efficiently. It is a powerful and popular plugin or feature supported by many code editors and integrated development environments (IDEs) like Visual Studio Code, Sublime Text, and others.
+
+Emmet allows developers to use short and intuitive abbreviations to generate HTML and CSS code, reducing the need for manual typing and improving productivity.
+
+### 2- What is difference between library and a framework ?
---
+++ 02-Igniting-our-App/script.js
@@ -0,0 +1,33 @@
+// const heading = React.createElement(
+//   "h2",
+//   {},
+//   "I have created an h2 tag using React Library"
+// );
+
+/*
+    <div id="parent"> 
+      <div id="child1">
+        <h1>I am an H1 Tag of child 1</h1>
+        <h1>I am an H2 Tag of child 1</h1>
+      </div>
+      <div id="child2">
+        <h1>I am an H1 Tag of child 2</h1>
+        <h1>I am an H2 Tag of child 2</h1>
+      </div>
+    </div>
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, " I am an H1 Tag of CHild 1"),
+    React.createElement("h2", {}, " I am an H2 Tag of CHild 1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, " I am an H1 Tag of CHild 2"),
+    React.createElement("h2", {}, " I am an H2 Tag of CHild 2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
