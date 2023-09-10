701d82eb28a88cf1cac3e6a221640d774ff820aa
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-09-11 01:09:01 +0530

    mid break

--- 03-Laying_the_foundations/script.js
+++ 03-Laying_the_foundations/script.js
@@ -1,36 +1,23 @@
 import React from "react";
-
 import ReactDOM from "react-dom/client";
-// const heading = React.createElement(
-//   "h2",
-//   {},
-//   "I have created an h2 tag using React Library"
-// );
 
-/*
-    <div id="parent"> 
-      <div id="child1">
-        <h1>I am an H1 Tag of child 1</h1>
-        <h1>I am an H2 Tag of child 1</h1>
-      </div>
-      <div id="child2">
-        <h1>I am an H1 Tag of child 2</h1>
-        <h1>I am an H2 Tag of child 2</h1>
-      </div>
-    </div>
-*/
+// React Element
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "This is Namaste React 🚀🌈"),
-    React.createElement("h2", {}, " I am an H2 Tag of CHild 1"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, " I am an H1 Tag of CHild 2"),
-    React.createElement("h2", {}, " I am an H2 Tag of CHild 2"),
-  ]),
-]);
+const para = React.createElement(
+  "p",
+  { id: "parent", style: { color: "white" } },
+  "This is a React Paragraph element."
+);
 
+console.log(para);
+// JSX Syntax
+
+const jsxHeading = (
+  <h1 id="parent" style={{ color: "skyblue" }}>
+    {" "}
+    Heading Created using JSX
+  </h1>
+);
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(jsxHeading);
